b96bbfca5b52d5d1c8fcfe4bfbd38a90b957d1c0
FeiFei Jin <dsf_jyf@qq.com>
2016-12-13 16:23:02 +0800

    添加footBar 按钮点击事件

--- www/js/controllers/footBarTempCtrl.js
+++ www/js/controllers/footBarTempCtrl.js
@@ -1,4 +1,11 @@
 module.controller('footBarTempCtrl', function ($scope, $ionicTabsDelegate, $state, $ionicGesture,
     $ionicSideMenuDelegate) {
+        
+    $scope.submit = function () {
+        $scope.data = "提交按钮被点击了。";
+    }
 
-    })
+    $scope.deny = function () {
+        $scope.data = "驳回按钮被点击了。";
+    }
+})
