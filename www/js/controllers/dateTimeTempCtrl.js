d964adb7e18793cfb0d7a808ce702ecd57275be8
FeiFei Jin <dsf_jyf@qq.com>
2016-12-30 11:36:45 +0800

    1,格式更改

--- www/js/controllers/dateTimeTempCtrl.js
+++ www/js/controllers/dateTimeTempCtrl.js
@@ -1,5 +1,10 @@
-module.controller('dateTimeTempCtrl', function ($scope, $ionicTabsDelegate, $state, $ionicGesture,
+module.controller('dateTimeTempCtrl', function($scope, $ionicTabsDelegate, $state, $ionicGesture,
     $ionicSideMenuDelegate) {
     $scope.dateValue = new Date();
     $scope.dateTimeValue = new Date();
+
+    $scope.$watch("dateValue", function(n, o) {
+        console.log(n);
+        console.log(o);
+    });
 });
